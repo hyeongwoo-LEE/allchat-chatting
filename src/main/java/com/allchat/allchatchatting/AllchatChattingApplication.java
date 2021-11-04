356eacccbc66b7112aad481873aa11b54c6f6f03
hyeongwoo-LEE <lhw970804@naver.com>
2021-11-04 19:40:47 +0900

    환경세팅 완료

---
+++ src/main/java/com/allchat/allchatchatting/AllchatChattingApplication.java
@@ -0,0 +1,13 @@
+package com.allchat.allchatchatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllchatChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllchatChattingApplication.class, args);
+	}
+
+}
